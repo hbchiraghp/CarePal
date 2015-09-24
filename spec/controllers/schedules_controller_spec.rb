1752fee5a6313083995dd78b2b21ba586dc0600a
shanmuk <hb.shanmuk@hiddenbrains.in>
2015-09-25 00:31:59 +0530

    Review and code cleanup

--- spec/controllers/schedules_controller_spec.rb
+++ spec/controllers/schedules_controller_spec.rb
@@ -1,21 +1,27 @@
-# require 'spec_helper'
-# describe SchedulesController do
-# 	let(:schedule) { FactoryGirl.create :schedule }
+require 'spec_helper'
+describe SchedulesController, :type => :controller do
+	let(:schedule) { FactoryGirl.create :schedule }
 
-# 	before(:each) do
-# 	    request.env["HTTP_REFERER"] = student_path(:student)
-#   	end
+	before(:each) do
+	    request.env["HTTP_REFERER"] = student_path(:student)
+  	end
 
-#   	context "Schedule For Student" do
-# 		before{ FactoryGirl.build(:student) }
-#   		it "POST /create" do
-# 	      	expect {
-# 	        	post :create, {
-# 	        	  { "schedule" => schedule }
-# 	        	}
-# 	      	}.to change{
-# 	        	Schedule.count
-# 	      }.by(1)
-# 	    end
-#   	end
-# end
+  	context "Schedule For Student" do
+		before{ FactoryGirl.build(:student) }
+  		it "POST /create" do
+	      	expect {
+	        	post :create, {"schedule" => {
+	        		"student" => :student,
+	        		"course" => "test",
+	        		"start_date" => "10-10-1986",
+	        		"end_date" => "10-10-1986",
+	        		"start_time" => "03:00",
+	        		"end_time" => "03:00",
+	        		"location" => "NewYork",
+	        		"faculty" => "Name1"
+	        		} }
+	      	}
+	      	 request.env["HTTP_REFERER"] = student_path(:student)
+	    end
+  	end
+end
